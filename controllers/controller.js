64b1f1b22d0a9739ad94a6717cc92ff0e0cbce99
jcsalinas20 <jcsalinasn20@gmail.com>
2022-01-05 13:11:27 +0100

    created end point get user

---
+++ controllers/controller.js
@@ -0,0 +1,30 @@
+const endpoint = require("../services/endPoints");
+const json = require("../services/json");
+const userModel = require("../models/user");
+
+/*** USER ***/
+
+// exports.updateUser = async (req, res) => {
+//   const user = await endpoint.getUser("jcsalinas20");
+//   userModel.findOneAndUpdate({ type: "user" }, json.user(user), (err, doc) => {
+//     if (doc) {
+//       res.header("Content-Type", "application/json");
+//       res.send(JSON.stringify({ status: true }, null, 2));
+//     } else {
+//       res.header("Content-Type", "application/json");
+//       res.send(JSON.stringify({ status: "Error 404" }, null, 2));
+//     }
+//   });
+// };
+
+exports.getUser = async (req, res) => {
+  userModel.findOne({ type: "user" }, function (err, doc) {
+    if (err) {
+      res.header("Content-Type", "application/json");
+      res.send(JSON.stringify({ status: "Error 404" }, null, 2));
+    } else {
+      res.header("Content-Type", "application/json");
+      res.send(JSON.stringify({ user: doc }, null, 2));
+    }
+  });
+};
